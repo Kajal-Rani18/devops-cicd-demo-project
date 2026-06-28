ee1daa8364a5cc0730cc70a2dd9704362571ae2f
Ubuntu <ubuntu@ip-172-31-19-62.ec2.internal>
2026-06-28 13:23:44 +0000

    Webhook Testing

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 http.createServer((req,res)=>{
-    res.end('Hello from Jenkins CI/CD\nStage1. Version 2 deployed automatically');
+    res.end('Hello from Jenkins CI/CD\nStage1. Version 2 deployed automatically\nStage2. Testing of the Webhook');
 }).listen(3000);
 
 
